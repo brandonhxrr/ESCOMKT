7b1b5e6aedd5e8a1442f61267f12158c99bcd589
Brandon Herrera <brandondh769@gmail.com>
2022-05-22 19:30:55 -0500

    Profile model added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False)
+    email = models.EmailField()
+    tel = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.user)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Profile
 
-# Create your views here.
+def homeView(request):
+    profile = Profile.objects.all()
+    context = {'profile':profile}
+    return render(request, 'home/home.html', context)
